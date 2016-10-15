010e4e123d52667e56817b9a4aedc21dfba987eb
Fabricio PH <fabriph@gmail.com>
2016-10-15 18:39:16 +0200

    Renamed analyze.

--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -5,13 +5,13 @@ function httpGet(theUrl) {
     return xmlHttp.responseText;
 }
 
-function click(e) {
+function clickAnalyze(e) {
   var responseTxt = httpGet("https://pier21.herokuapp.com/article?url=http://www.bbc.com/news/science-environment-37665529&disable_text=1");
     var response = eval('(' + responseTxt + ')')
   document.getElementById("score").innerHTML = response["relevancy"]
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-  var button = document.getElementById("analyze");
-  button.addEventListener('click', click);
+  var buttonAnalyze = document.getElementById("analyze");
+  buttonAnalyze.addEventListener('click', clickAnalyze);
 });
