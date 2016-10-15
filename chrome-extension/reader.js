29eeae89a4806a8b0c272902f32f1ad167969d21
Fabricio PH <fabriph@gmail.com>
2016-10-15 15:41:18 +0200

    Added content crawler for looking the right content of the email to be send to analyze.

--- chrome-extension/reader.js
+++ chrome-extension/reader.js
@@ -1,10 +1,48 @@
 addJSInjectedButton();
 
+function crawlForContent(node) {
+  if (!node) {
+    return;
+  }
+  if (node.tagName &&
+      (node.tagName.toLowerCase() == 'input' || node.tagName.toLowerCase() == 'textarea')) {
+    return;
+  }
+  if (node.classList && node.classList.contains('ace_editor')) {
+    return;
+  }
+
+  var child, next;
+  switch (node.nodeType) {
+      case 1:  // Element
+      case 9:  // Document
+      case 11: // Document fragment
+          child = node.firstChild;
+          while (child) {
+              next = child.nextSibling;
+              crawlForContent(child);
+              child = next;
+          }
+          break;
+
+      case 3: // Text node
+          requestClassification(node);
+          break;
+    }
+}
+
+function requestClassification(textNode) {
+  var v = textNode.nodeValue;
+  v = v.replace(/ola/g, "AAAA");
+  textNode.nodeValue = v;
+}
+
 function addJSInjectedButton() {
-  var text = document.createTextNode("You've been JS injected");
+  var text = document.createTextNode("Inject JS");
   var btn = document.createElement("BUTTON")
   btn.onclick = function () {
-    window.location='https://github.com/THEPortatCERN/Hackathon2016-Pier21';
+    crawlForContent(document.body);
+    alert('Done');
   };
   btn.appendChild(text);
   document.body.appendChild(btn);
