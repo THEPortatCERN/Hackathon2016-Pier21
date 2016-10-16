88814d2334abc83d514da8f3a91dc57554197db6
Fabricio PH <fabriph@gmail.com>
2016-10-16 15:15:30 +0200

    Posting message of Keywords from popup.js to background.js

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -3,8 +3,16 @@ chrome.extension.onConnect.addListener(function(port) {
   console.log("Connected");
   port.onMessage.addListener(function(msg) {
     console.log("background.js message recieved " + msg);
-    chrome.tabs.query({active : true, currentWindow: true}, function (tabs) {
-      port.postMessage(tabs[0].url);
-    });
+    if (msg == "request-current-URL") {
+      chrome.tabs.query({active : true, currentWindow: true}, function (tabs) {
+        port.postMessage(tabs[0].url);
+      });
+    } else {
+      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {keywords: msg}, function(response) {
+          chrome.extension.getBackgroundPage().console.log("all cool");
+        });
+      });
+    }
   });
 });
